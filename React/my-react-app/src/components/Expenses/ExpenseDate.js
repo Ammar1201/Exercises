5f1c7c0b8356094f0703bb3b5df96ff4cee6952a
Ammar1201 <aamour1201@gmail.com>
2022-11-07 22:32:41 +0200

    my-react-app added filtering expenses function

--- React/my-react-app/src/components/Expenses/ExpenseDate.js
+++ React/my-react-app/src/components/Expenses/ExpenseDate.js
@@ -1,14 +1,14 @@
 import './ExpenseDate.css';
 
 const ExpenseDate = (props) => {
-  // const month = props.date.toLocaleDateString('en-US', { month: 'long' });
   // const day = props.date.toLocaleDateString('en-US', { day: '2-digits' });
   // const year = props.date.getFullYear();
-
+  
   //! ToLocaleDateString method didn't work for some reason.
-
-  const month = props.date.getUTCMonth();
+  
+  // const month = props.date.getUTCMonth();
   const day = props.date.getUTCDay();;
+  const month = props.date.toLocaleDateString('en-US', { month: 'long' });
   const year = props.date.getFullYear();
   
   return (
