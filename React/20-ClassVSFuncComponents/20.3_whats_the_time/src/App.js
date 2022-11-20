7b615405157f7cd4e7312066c52a8bab39cfc8ee
Ammar1201 <aamour1201@gmail.com>
2022-11-20 14:45:02 +0200

    added 20.3_whats_the_time exercise

---
+++ React/20-ClassVSFuncComponents/20.3_whats_the_time/src/App.js
@@ -0,0 +1,7 @@
+import Inputs from "./components/Inputs";
+
+const App = () => {
+  return <Inputs />;
+}
+
+export default App;
---
+++ React/20-ClassVSFuncComponents/20.3_whats_the_time/src/components/Inputs.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+class Inputs extends Component {
+  state = {seconds: '', minutes: '', hours: ''}; 
+
+  // secondsChanged() {
+
+  // }
+
+  // minutesChanged() {
+
+  // }
+
+  // hoursChanged() {
+
+  // }
+
+  handleChange({target}) {
+    const time = {};
+    switch(target.id) {
+      case'seconds':
+        time.seconds = target.value;
+        time.minutes = target.value / 60 || '';
+        time.hours = target.value / 60 / 60 || '';
+        break;
+      case 'minutes':
+        time.seconds = target.value * 60 || '';
+        time.minutes = target.value;
+        time.hours = target.value / 60 || '';
+        break;
+      case 'hours':
+        time.seconds = target.value * 60 * 60 || '';
+        time.minutes = target.value * 60 || '';
+        time.hours = target.value;
+        break;
+      default:
+        break;
+    }
+    this.setState(time);
+  }
+
+  render() { 
+    return (
+      <div>
+        <div>
+          <label>Seconds:</label>
+          <input id='seconds' type="text" value={this.state.seconds} onChange={this.handleChange.bind(this)} />
+        </div>
+        <div>
+          <label>Minutes:</label>
+          <input id='minutes' type="text" value={this.state.minutes} onChange={this.handleChange.bind(this)} />
+        </div>
+        <div>
+          <label>Hours:</label>
+          <input id='hours' type="text" value={this.state.hours} onChange={this.handleChange.bind(this)} />
+        </div>
+      </div>
+    );
+  }
+}
+ 
+export default Inputs;
