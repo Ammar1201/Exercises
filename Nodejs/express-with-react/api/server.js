11c6df42da4ce0a4461d607f554f55eeb7b1f578
Ammar1201 <aamour1201@gmail.com>
2022-12-07 21:18:59 +0200

    express-with-react moved react-app to client folder

--- Nodejs/express-with-react/api/server.js
+++ Nodejs/express-with-react/api/server.js
@@ -4,13 +4,15 @@ import cors from 'cors';
 const PORT = 5000;
 const app = express();
 
+const weatherApiBaseUrl = 'http://api.weatherapi.com/v1/current.json?key=794c127444ab46f8a84163836220712';
+
 app.use(json());
 app.use(cors());
 
 app.get('/current?:city', async (req, res) => {
   try {
     const { city } = req.query;
-    const response = await fetch('http://api.weatherapi.com/v1/current.json?key=794c127444ab46f8a84163836220712&q=' + city);
+    const response = await fetch(`${weatherApiBaseUrl}&q=${city}`);
     const data = await response.json();
     res.status(200).send(JSON.stringify(data));
   }
