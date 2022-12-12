f11f40229a904ffd8a15f530319d1970aac8710a
Ammar1201 <aamour1201@gmail.com>
2022-12-12 22:04:21 +0200

    added 17.1-product_model exercise solved

---
+++ Nodejs/17-MongooseModel/17.1-product_model/mongoose.js
@@ -0,0 +1,108 @@
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://127.0.0.1:27017/shop', {
+  useNewUrlParser: true
+});
+
+const Product = mongoose.model('Product', {
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  isActive: {
+    type: Boolean
+  },
+  details: {
+    description: {
+      type: String,
+      minlength: 10
+    },
+    price: {
+      type: Number,
+      required: true,
+      validate(value) {
+        if (value < 0) {
+          throw new Error('Price must be a positive number!')
+        }
+      }
+    },
+    discount: {
+      type: Number,
+      default: 0
+    },
+    imgs: {
+      type: Array,
+      validate(value) {
+        if (value.length < 2) {
+          throw new Error('images array must include at least two images');
+        }
+      }
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (value.length !== 10) {
+          throw new Error('phone number must be 10 numbers');
+        }
+
+        if (value[0] !== '0') {
+          throw new Error('number should start with zero');
+        }
+
+        if (value[2] !== '0') {
+          throw new Error('third character should be zero');
+        }
+      }
+    },
+    dateAdded: {
+      type: Date,
+      default: new Date()
+    }
+  }
+});
+
+const product1 = new Product({
+  name: 'gaming pc',
+  category: 'electronics',
+  isActive: true,
+  details: {
+    description: 'the best gaming pc you will ever see',
+    price: 2500,
+    imgs: ['img1', 'img2'],
+    phoneNumber: '0501234567'
+  }
+});
+
+const product2 = new Product({
+  name: 'gaming chair',
+  category: 'furniture',
+  isActive: true,
+  details: {
+    description: 'the best gaming chair you will ever see',
+    price: 1000,
+    imgs: ['img1', 'img3', 'img5'],
+    phoneNumber: '0507654321'
+  }
+});
+
+const product3 = new Product({
+  name: 'gaming headset',
+  category: 'electronics',
+  isActive: false,
+  details: {
+    description: 'the best gaming headset you will ever see',
+    price: 500,
+    discount: 50,
+    imgs: ['img', 'img', 'img'],
+    phoneNumber: '0501973256'
+  }
+});
+
+product1.save().then((product) => console.log(product)).catch((error) => console.log(error));
+product2.save().then((product) => console.log(product)).catch((error) => console.log(error));
+product3.save().then((product) => console.log(product)).catch((error) => console.log(error));
